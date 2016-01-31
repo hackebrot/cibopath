1d6ddc15fd6b195f3331e8c4022f0760da0fe9ef
Raphael Pierzina <raphael@hackebrot.de>
2016-01-31 16:40:43 +0000

    Remove unwanted whitespace from doc str

--- cibopath/cli.py
+++ cibopath/cli.py
@@ -8,7 +8,7 @@ from cibopath import __version__
 @click.group()
 @click.version_option(__version__, u'-V', u'--version', prog_name='Cibopath')
 def cli():
-    """Cibopath -  Search Cookiecutters on GitHub."""
+    """Cibopath - Search Cookiecutters on GitHub."""
 
 
 @cli.command('update')
