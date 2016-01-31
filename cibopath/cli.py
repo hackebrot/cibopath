84cec2e4635560c8cdf9ed8ecb165b1b672e92f7
Raphael Pierzina <raphael@hackebrot.de>
2016-01-31 16:33:00 +0000

    Implement click along with a pytest cli tests

---
+++ cibopath/cli.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import click
+
+from cibopath import __version__
+
+
+@click.group()
+@click.version_option(__version__, u'-V', u'--version', prog_name='Cibopath')
+def cli():
+    """Cibopath -  Search Cookiecutters on GitHub."""
+
+
+@cli.command('update')
+def update_cmd():
+    print('update')
+
+main = cli
---
+++ tests/test_cli.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from click.testing import CliRunner
+import pytest
+
+from cibopath.cli import main
+
+runner = CliRunner()
+
+
+@pytest.fixture(params=['-V', '--version'])
+def version_cli_flag(request):
+    return request.param
+
+
+def test_cli_version(version_cli_flag):
+    result = runner.invoke(main, [version_cli_flag])
+    assert result.exit_code == 0
+    assert result.output.startswith('Cibopath')
+
+
+def test_update():
+    result = runner.invoke(main, ['update'])
+    assert result.exit_code == 0
+    assert result.output.startswith('update')
