6709944d7e856fbce0434da0dc731fc83b55feb1
Raphael Pierzina <raphael@hackebrot.de>
2016-03-06 14:01:09 +0000

    Extend integration test to check correctness of dumped json data

--- tests/test_cli_update.py
+++ tests/test_cli_update.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import pathlib
+import json
 
 
-def test_should_write_json(cli_runner, tmp_rc, tmp_templates_file):
+def test_store_template_data_to_json(cli_runner, tmp_rc, tmp_templates_file):
     result = cli_runner([
         '-c', tmp_rc, 'update'
     ])
@@ -12,3 +13,15 @@ def test_should_write_json(cli_runner, tmp_rc, tmp_templates_file):
 
     templates = pathlib.Path(tmp_templates_file)
     assert templates.exists()
+
+    with templates.open('r', encoding='utf8') as fh:
+        template_data = json.load(fh)
+
+    fetched_templates = [template['name'] for template in template_data]
+    expected_templates = [
+        'cookiecutter-pypackage',
+        'cookiecutter-pylibrary',
+        'cookiecutter-pytest-plugin',
+        'cookiecutter-tapioca',
+    ]
+    assert fetched_templates == expected_templates
