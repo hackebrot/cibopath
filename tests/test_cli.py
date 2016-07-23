37a0cb41a88114ab9edb514e29447756b0c3e92a
Raphael Pierzina <raphael@hackebrot.de>
2016-07-23 15:17:05 +0200

    Use cli_runner fixture in test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from click.testing import CliRunner
 import pytest
 
-from cibopath.cli import main
 from cibopath import __version__
-
-runner = CliRunner()
 
 
 @pytest.fixture(params=['-V', '--version'])
@@ -14,7 +10,7 @@ def version_cli_flag(request):
     return request.param
 
 
-def test_cli_group_version_option(version_cli_flag):
-    result = runner.invoke(main, [version_cli_flag])
+def test_cli_group_version_option(cli_runner, version_cli_flag):
+    result = cli_runner([version_cli_flag])
     assert result.exit_code == 0
     assert result.output == 'cibopath, version {}\n'.format(__version__)
